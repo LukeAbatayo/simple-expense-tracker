4a99217dde10b200fca8c7faafb100a95d1ffe5f
Luke Abatayo <47374947+LukeAbatayo@users.noreply.github.com>
2021-07-06 23:15:33 +0800

    added key attribute on ExpenseItem mapping

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,9 +14,13 @@ const Expenses = (props) => {
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesFilter 
+        selected={filteredYear} 
+        onChangeFilter={filterChangeHandler} 
+      />
       {props.items.map(expense => (
-        <ExpenseItem         
+        <ExpenseItem
+          key={expense.id}
           title={expense.title} 
           amount={expense.amount} 
           date={expense.date}
